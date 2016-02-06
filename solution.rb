7b4f52098d21db9a84b5ea10ce35e728cfb7a860
lenin fernando <ingeleninfernando@gmail.com>
2016-02-06 15:01:29 -0500

    Update solution.rb

--- solution.rb
+++ solution.rb
@@ -1,10 +1,27 @@
 
 
+
 require 'sinatra'
 
 
+get '/' do
+	unless params[:nombre]
+		"Hola desconocido"
+
+	else
+
+		"Hola #{params[:nombre]}"
+
+	end
+
+end
+
+
 get '/:nombre' do
-"Hola #{params[:nombre]}"
+
+	"Hola #{params[:nombre]}"
+
+
 
 
 end
