d9c421ccd09f138798198b4e283e03aaeaf62b6f
lenin fernando <ingeleninfernando@gmail.com>
2016-02-09 18:18:44 -0500

    Update solution.rb

--- solution.rb
+++ solution.rb
@@ -6,48 +6,9 @@ require 'sinatra'
 get '/maker/:name' do
   
 
-  "Hola #{params[:name].capitalize}"
+  "Hola #{params[:name].capitalize}!"
 end
 
 
 
 
-
-=begin
-require 'sinatra'
-require 'sinatra-arg'
-
-get '/maker/:name' do
-  erb "Hello #{arg(1).capitalize}"
-end
-
-
-
-=begin
-
-
-require 'sinatra'
-
-
-get '/' do
-	unless params[:nombre]
-		"Hola desconocido"
-
-	else
-
-		"Hola #{params[:nombre]}"
-
-	end
-
-end
-
-
-get '/:nombre' do
-
-	"Hola #{params[:nombre]}"
-
-
-
-
-end
-=end
